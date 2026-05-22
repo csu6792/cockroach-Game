1dc4f7a51c2f8e2eaf6162456413f7bc60e39400
csu6792 <53500308+csu6792@users.noreply.github.com>
2026-05-23 01:42:32 +0800

    Update main.js

--- main.js
+++ main.js
@@ -144,9 +144,9 @@ let isBossMode = false; // 標記當前是否為 BOSS 挑戰模式
 //setInterval(checkBossEvent, 1000);
 //window.addEventListener('DOMContentLoaded', checkBossEvent);
 
-function startBossMode() {
+/*function startBossMode() {
     alert("即將開放");
-}
+}*/
 
         // ==========================================
         // PVP State Variables
